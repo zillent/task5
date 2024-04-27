c9206fb6f9e39ba97c869195c700f064c42241e2
zillent <serg@zillent.ru>
2024-04-27 13:41:39 +0500

    add search method to AccountRepository

--- src/main/java/ru/zillent/study/task5/common/model/AccountRepository.java
+++ src/main/java/ru/zillent/study/task5/common/model/AccountRepository.java
@@ -3,6 +3,9 @@ package ru.zillent.study.task5.common.model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findAnyByAccountPoolId(Long accountPoolId);
 }
